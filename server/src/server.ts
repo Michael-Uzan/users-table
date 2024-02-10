5283f66f92ee380c377f310e3caca7d53608b448
Michael Uzan <looply3@gmail.com>
2024-02-10 15:15:37 +0200

    feat_backend: preperation for deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,13 +14,11 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.static("public"));
 
 if (process.env.NODE_ENV === "production") {
-  port = "port on production";
-  host = "host production";
+  port = process.env.PRODUCTION_PORT;
   // Express serve static files on production environment
-  app.use(express.static(path.resolve(__dirname, "src")));
+  app.use(express.static(path.resolve(__dirname, "public")));
 } else {
   port = process.env.BACK_PORT;
-  host = process.env.BACK_HOST;
   const corsOptions = {
     // Make sure origin contains the url your frontend is running on
     origin: ["http://127.0.0.1:3000", "http://localhost:3000"],
@@ -31,5 +29,5 @@ if (process.env.NODE_ENV === "production") {
 app.use("/api/users", usersRoutes);
 
 app.listen(port, () => {
-  console.log(`server running : http://${host}:${port}`);
+  console.log(`server running : http://${process.env.BACK_HOST}:${port}`);
 });
